f1a217dcbb474c36fa5f72e596e7be9c4e785eed
Malwina Prutis <prutis@vp.pl>
2024-10-15 13:19:35 +0200

    Update script.js
    braces and arrow functions added

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
-const buttonDelImgElement = document.querySelector(".js-buttonDeletePhoto");
-const imgDelElement = document.querySelector(".js-image");
+{
+  const welcome = () => {
+    console.log("Welcome!");
+  };
+  const buttonDelImgElement = document.querySelector(".js-buttonDeletePhoto");
+  const imgDelElement = document.querySelector(".js-image");
 
-buttonDelImgElement.addEventListener("click", () => {
-  imgDelElement.remove();
-  buttonDelImgElement.remove();
-});
+  buttonDelImgElement.addEventListener("click", () => {
+    imgDelElement.remove();
+    buttonDelImgElement.remove();
+  });
 
-const buttonChangeBackgroundElement = document.querySelector(
-  ".js-buttonChangeBackground"
-);
-const bodyElement = document.querySelector("body"); // Poprawione: zmieniłem ".body" na "body"
-let buttonChangeBackgroundText = document.querySelector(
-  ".js-buttonChangeBackgroundText"
-);
+  const buttonChangeBackgroundElement = document.querySelector(
+    ".js-buttonChangeBackground"
+  );
+  const bodyElement = document.querySelector("body");
+  let buttonChangeBackgroundText = document.querySelector(
+    ".js-buttonChangeBackgroundText"
+  );
 
-buttonChangeBackgroundElement.addEventListener("click", () => {
-  bodyElement.classList.toggle("body--background");
+  buttonChangeBackgroundElement.addEventListener("click", () => {
+    bodyElement.classList.toggle("body--background");
 
-  buttonChangeBackgroundText.innerText = bodyElement.classList.contains(
-    "body--background"
-  )
-    ? "Przywróć oryginalne tło"
-    : "Zmień tło";
-});
-const buttonGoToTopElement = document.querySelector(".js-buttonGoToTop");
+    buttonChangeBackgroundText.innerText = bodyElement.classList.contains(
+      "body--background"
+    )
+      ? "Przywróć oryginalne tło"
+      : "Zmień tło";
+  });
+  const buttonGoToTopElement = document.querySelector(".js-buttonGoToTop");
 
-buttonGoToTopElement.addEventListener("click", () => {
-  scroll(0, 0);
-});
+  buttonGoToTopElement.addEventListener("click", () => {
+    scroll(0, 0);
+  });
+}
